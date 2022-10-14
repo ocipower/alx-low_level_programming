7b6bcc57ad8700471926408c57635bbc7d95c586
Ocipower <Ocipowerlight@gmail.com>
2022-10-13 20:30:35 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The entry point of my program
+ * Return: returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
