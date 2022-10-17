f18596447e9866d945706a297ed213c0ec6e3d97
Ocipower <Ocipowerlight@gmail.com>
2022-10-16 20:35:47 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+* main - enrty point
+* Return: Always 0
+*/
+int main(void)
+{
+	int start = 97;
+	int stop = 122;
+	int i = 0;
+
+	for (i = start; i <= stop; i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+	return (0);
+}
+
+
