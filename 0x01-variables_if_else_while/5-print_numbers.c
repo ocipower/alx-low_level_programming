3aef7c13038af2db030aacf402b1564d743661a1
Ocipower <Ocipowerlight@gmail.com>
2022-10-16 21:44:18 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+  * main - entry point
+  * Return: returns 0
+  */
+int main(void)
+{
+	int i;
+	int num = 10;
+
+	for (i = 0; i < num; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
+
