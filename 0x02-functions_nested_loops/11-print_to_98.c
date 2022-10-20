8ee189a85990f4640f5a5d04f9b9add6b0ebb7b2
Ocipower <Ocipowerlight@gmail.com>
2022-10-19 20:26:33 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * print_to_98 - prints natural numbers upt 38
+  * @n: input param
+  */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
