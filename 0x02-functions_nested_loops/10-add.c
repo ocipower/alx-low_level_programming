6309b38aedf0b673f2c9d9ef1efb355789eb2d7b
Ocipower <Ocipowerlight@gmail.com>
2022-10-19 19:47:02 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * add - adds two integers
+  * @num1: input param
+  * @num2: input param
+  * Return: sume of two intwegers (success)
+  */
+
+int add(int num1, int num2)
+{
+
+	return (num1 + num2);
+}
