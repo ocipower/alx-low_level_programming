2542b15d9e55b01eb75f4b570fad797ce0c6c7a5
Ocipower <Ocipowerlight@gmail.com>
2022-10-21 15:23:47 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - multiplies two numbers
+  * @a: input param
+  * @b: input param
+  * Return : product on success
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
