1f7407526685c6c8a0bc66f4894bcc4ae02eb85e
Ocipower <Ocipowerlight@gmail.com>
2022-10-25 18:27:31 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - printa character array
+ * @str: input charater to be printed
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
