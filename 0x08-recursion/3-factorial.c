10e5b5439f2c56455081b8966246da6f68d7cb98
Ocipower <Ocipowerlight@gmail.com>
2022-11-03 19:26:14 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * factorial - returns the factorial of a given number.
+  * @n: n factoral param
+  * Return: returns factorial
+  */
+
+int factorial(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
