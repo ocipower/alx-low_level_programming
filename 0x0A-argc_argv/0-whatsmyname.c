fe0dffb63a45ca50a3af339a0405a4e413e19cdf
Ocipower <Ocipowerlight@gmail.com>
2022-11-09 01:29:17 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+  * main - entry point
+  * @argc: args count
+  * @argv: args values
+  * Return: Always 0
+  */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", *argv);
+	return (0);
+}
