1ef66d894160b6c1192582ef012fc7a39051d221
Ocipower <Ocipowerlight@gmail.com>
2022-11-15 07:54:30 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+  * struct dog - dog class
+  *
+  * @name: dog's name
+  *
+  * @owner: dog owner
+  *
+  * @age: dogs age
+  */
+struct dog
+{
+	char *name;
+	loat age;
+	char *owner;
+};
+
+#endif
