eac7196522caa7c071b65c5d0d021fd1a1d0b124
Ocipower <Ocipowerlight@gmail.com>
2022-12-02 13:59:47 -0800

    task 100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,24 +1,16 @@
 #include "main.h"
 /**
- * flip_bits - flip to get from one number to another.
- * @n: the first number
- * @m: the second number
- * Return: the number of bits you would need to flip
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+int get_endianness(void)
 {
-	unsigned long int differnce, result;
-	unsigned int j, i;
+	int i;
+	char *c;
 
-	j = 0;
-	result = 1;
-	differnce = n ^ m;
-	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
-	{
-		if (result == (differnce & result))
-			j++;
-		result <<= 1;
-	}
+	i = 1;
+	c = (char *)&i;
 
-	return (j);
+	return (*c);
 }
