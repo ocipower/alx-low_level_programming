8c4836690089a58164979f0d5f0f65f116e599a1
Ocipower <ocipowerlight@gmail.com>
2023-01-20 06:10:29 -0800

    ammended task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+  * print_dlistint - prints list.
+  * @h: pointer to head node.
+  * Return: number of nodes printed
+  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int no_of_nodes;
@@ -18,4 +24,5 @@ size_t print_dlistint(const dlistint_t *h)
 			no_of_nodes++;
 		}
 	}
+	return (no_of_nodes);
 }
