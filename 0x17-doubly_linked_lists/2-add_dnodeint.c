bc8f4482b50542fc3fb8117e38392a1ac2a22905
Ocipower <ocipowerlight@gmail.com>
2023-01-21 06:34:49 -0800

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+  * add_dnodeint - add node to the beginning of of a dlistint_t list
+  * @head: head node
+  * @n: int element to add
+  * Return: address of new node on success else reutun NULL 
+  */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node)
+	{
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = *head;
+	}
+	else
+		return (NULL);
+	if (*head)
+		(*head)->prev = new_node;
+	*head = new_node;
+	return (*head);
+}
