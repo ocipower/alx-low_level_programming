469de6ba556394ccd83beb8f8d2971eca1733f26
Ocipower <ocipowerlight@gmail.com>
2023-01-20 21:24:07 -0800

    task 3

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * mod - modulo of two numbers
+  * @a: first rand num
+  * @b: second rand num
+  * Return: mod of a and b
+  */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
